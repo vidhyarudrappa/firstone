0462b8f90c14a9cd664a86899cbfe4d826e1f402
vidhyarudrappa <47228161+vidhyarudrappa@users.noreply.github.com>
2021-07-14 19:27:59 -0700

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello this my first cloud computing project")
+
+
+# # This is the Body of this code
+
+# In[36]:
+
+
+def add1(x,y):
+    print(f'the addition of {x} and {y}')
+    return x + y
+
+
+# In[37]:
+
+
+add1(1,2)
+
+
+# In[ ]:
+
+
+
+
